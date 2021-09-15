34c739023e889fdf71cd4e3172ac0f5f0ba28c58
matias <matias3282@hotmail.com>
2021-09-15 20:32:09 -0300

    dom-desafio

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEL = document.querySelector(".lista");
+  const itemsEls = document.querySelectorAll(".lista li");
+
+  for (const el of itemsEls) {
+    el.remove();
+  }
+  for (const item of cosasQueAprendimos) {
+    const newLiEL = document.createElement("li");
+    newLiEL.textContent = item.tema;
+    const laClaseQueLeVoyAAsignarAlLi = item.tema || "item";
+    newLiEL.classList.add(laClaseQueLeVoyAAsignarAlLi);
+    listaEL.appendChild(newLiEL);
+  }
+}
 
 main();
